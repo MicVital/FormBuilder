e9c2600ee5e5398fd74b4faa99effec333ee5ce8
MicVital <31258656+MicVital@users.noreply.github.com>
2020-04-23 07:10:53 +0000

    Fixed the router  ommision
    backend route was lacking the $router-> pointer

--- Http/backendRoutes.php
+++ Http/backendRoutes.php
@@ -5,14 +5,14 @@ use Illuminate\Routing\Router;
 /* @var Router $router */
 
 $router->group(['prefix' => '/formbuilder'], function (Router $router) {
-    get('formbuilder', ['as' => 'admin.formbuilder.formbuilder.index', 'uses' => 'FormbuilderController@index']);
-    get('formbuilder/create', ['as' => 'admin.formbuilder.formbuilder.create', 'uses' => 'FormbuilderController@create']);
-    post('formbuilder', ['as' => 'admin.formbuilder.formbuilder.store', 'uses' => 'FormbuilderController@store']);
-    get('formbuilder/{form}/edit', ['as' => 'admin.formbuilder.formbuilder.edit', 'uses' => 'FormbuilderController@edit']);
-    put('formbuilder/{form}/edit', ['as' => 'admin.formbuilder.formbuilder.update', 'uses' => 'FormbuilderController@update']);
-    delete('formbuilder/{form}', ['as' => 'admin.formbuilder.formbuilder.destroy', 'uses' => 'FormbuilderController@destroy']);
+    $router->get('formbuilder', ['as' => 'admin.formbuilder.formbuilder.index', 'uses' => 'FormbuilderController@index']);
+    $router->get('formbuilder/create', ['as' => 'admin.formbuilder.formbuilder.create', 'uses' => 'FormbuilderController@create']);
+    $router->post('formbuilder', ['as' => 'admin.formbuilder.formbuilder.store', 'uses' => 'FormbuilderController@store']);
+    $router->get('formbuilder/{form}/edit', ['as' => 'admin.formbuilder.formbuilder.edit', 'uses' => 'FormbuilderController@edit']);
+    $router->put('formbuilder/{form}/edit', ['as' => 'admin.formbuilder.formbuilder.update', 'uses' => 'FormbuilderController@update']);
+    $router->delete('formbuilder/{form}', ['as' => 'admin.formbuilder.formbuilder.destroy', 'uses' => 'FormbuilderController@destroy']);
 
-    get('submission/', ['as' => 'admin.formbuilder.submissions.index', 'uses' => 'SubmissionController@index']);
-    get('submission/{form}', ['as' => 'admin.formbuilder.submissions.form', 'uses' => 'SubmissionController@form']);
-    delete('submission/{form}', ['as' => 'admin.formbuilder.submissions.destroy', 'uses' => 'SubmissionController@destroy']);
+    $router->get('submission/', ['as' => 'admin.formbuilder.submissions.index', 'uses' => 'SubmissionController@index']);
+    $router->get('submission/{form}', ['as' => 'admin.formbuilder.submissions.form', 'uses' => 'SubmissionController@form']);
+    $router->delete('submission/{form}', ['as' => 'admin.formbuilder.submissions.destroy', 'uses' => 'SubmissionController@destroy']);
 });
