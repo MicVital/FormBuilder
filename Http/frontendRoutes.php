a7becca637d312764d8ce06985e91176bc4ab427
MicVital <31258656+MicVital@users.noreply.github.com>
2020-04-23 07:21:03 +0000

    Appended $router to the routes

--- Http/frontendRoutes.php
+++ Http/frontendRoutes.php
@@ -3,7 +3,7 @@
 use Illuminate\Routing\Router;
 
 /* @var Router $router */
-$router->group(['prefix' => '/formbuilder'], function () {
-    post('send', ['as' => 'front.formbuilder.formbuilder.send', 'uses' => 'FormController@send']);
-    get('captcha', ['as' => 'front.formbuilder.formbuilder.captcha', 'uses' => 'FormController@getCaptcha']);
+$router->group(['prefix' => '/formbuilder'], function (Router $router) {
+    $router->post('send', ['as' => 'front.formbuilder.formbuilder.send', 'uses' => 'FormController@send']);
+    $router->get('captcha', ['as' => 'front.formbuilder.formbuilder.captcha', 'uses' => 'FormController@getCaptcha']);
 });
